004419badedb935ba435f3dd3e4968d7f60ab8a2
root <root@DESKTOP-DH9FK14.localdomain>
2024-06-30 13:43:11 -0600

    added routing and base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import ToDo from "./components/ToDo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>to do</h1>
+      <div className="text-center">
+        <h1 className="bold">to do</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/todo/:id" element={<ToDo />} />
+        </Routes>
       </div>
     </>
   );
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const ToDo = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <p>this is to do</p>
+      <p>Id = {id}</p>
+    </div>
+  );
+};
+
+export default ToDo;
